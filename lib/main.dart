69a71b0dfdad784434a86811edf1ef7fc7ee5ea0
shiori309 <cindyshiori.k@gmail.com>
2026-01-02 18:39:57 +0900

    Set up app entry point

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Phrase App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+      ),
+      //home: const MainNavigation()
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+    );
+  }
+}
